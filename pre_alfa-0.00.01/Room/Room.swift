eba77656f20f143d44a45fe3213b5e3a1a1480ed
nersonSwift <42254802+nersonSwift@users.noreply.github.com>
2018-10-08 19:52:36 +0300

    1

--- pre_alfa-0.00.01/Room/Room.swift
+++ pre_alfa-0.00.01/Room/Room.swift
@@ -11,24 +11,104 @@ import RealmSwift
 
 
 class Room {
-    /*
-    var id = 0
-    var firstVisitingTriger = true
-    var nameRoom = ""
-    var close = false
-    var inRoom = false
+    
+    var saveRoom = SaveRoom()
+    
+    var realm: Realm?
+    var savedRooms: Results<SaveRoom>!
+    
+    var id: Int{
+        set(id) {
+            saveRoom.id = id
+            saveThisRoom(realm: realm, sevedRoom: savedRooms)
+        }
+        get{
+            return saveRoom.id
+        }
+    }
+    var firstVisitingTriger: Bool{
+        set(firstVisitingTriger) {
+            saveRoom.firstVisitingTriger = firstVisitingTriger
+            saveThisRoom(realm: realm, sevedRoom: savedRooms)
+        }
+        get{
+            return saveRoom.firstVisitingTriger
+        }
+    }
+    var nameRoom : String{
+        set(nameRoom) {
+            saveRoom.name = nameRoom
+            saveThisRoom(realm: realm, sevedRoom: savedRooms)
+        }
+        get{
+            return saveRoom.name
+        }
+    }
+    var close: Bool{
+        set(firstVisitingTriger) {
+            saveRoom.close = close
+            saveThisRoom(realm: realm, sevedRoom: savedRooms)
+        }
+        get{
+            return saveRoom.close
+        }
+    }
+    var x: Int{
+        set(x) {
+            saveRoom.x = x
+            saveThisRoom(realm: realm, sevedRoom: savedRooms)
+        }
+        get{
+            return saveRoom.x
+        }
+    }
+    var y: Int{
+        set(y) {
+            saveRoom.y = y
+            saveThisRoom(realm: realm, sevedRoom: savedRooms)
+        }
+        get{
+            return saveRoom.y
+        }
+    }
+    var inRoom : Bool{
+        set(inRoom) {
+            saveRoom.inRoom = inRoom
+            saveThisRoom(realm: realm, sevedRoom: savedRooms)
+        }
+        get{
+            return saveRoom.inRoom
+        }
+    }
+ 
     var enemys: [Enemy] = []
-    var x = 0
-    var y = 0
     var xy: String{
         return String(x) + String(y)
     }
-    */
+    var Doors: [String: Door]{
+        set(doors){
+            for i in doors{
+                switch i.key{
+                case "Up": saveRoom.up = i.value.rawValue
+                case "Right": saveRoom.right = i.value.rawValue
+                case "Down": saveRoom.down = i.value.rawValue
+                case "Left": saveRoom.left = i.value.rawValue
+                default:break
+                }
+            }
+        }
+        get{
+            var doors: [String: Door] = [:]
+            doors["Up"]     = GetClass.getDoor(name: saveRoom.up)
+            doors["Right"]  = GetClass.getDoor(name: saveRoom.right)
+            doors["Down"]   = GetClass.getDoor(name: saveRoom.down)
+            doors["Left"]   = GetClass.getDoor(name: saveRoom.left)
+            return doors
+        }
+    } // = ["Up" : Door.noDoor, "Right" : Door.noDoor, "Down" : Door.noDoor, "Left" : Door.noDoor]
     
+    init(){}
     
-    var Doors = ["Up" : Door.noDoor, "Right" : Door.noDoor, "Down" : Door.noDoor, "Left" : Door.noDoor]
-    
-    init() {}
     func openRoom(player: Player) -> Bool{return true}
     
     func loadRoom(saveRoom: SaveRoom, castPlayer: CastPlayer){
@@ -74,61 +154,27 @@ class Room {
         }
     }
     
-    func saveThisRoom(realm: Realm, sevedRoom: Results<SaveRoom>!){
-        let saveRoom = SaveRoom()
-
-       
+    func saveThisRoom(realm: Realm?, sevedRoom: Results<SaveRoom>!){
+     //   let saveRoom = SaveRoom()
+       // self.realm = realm
+       // self.sevedRoom = sevedRoom
+        if (sevedRoom == nil) || (realm == nil){
+            return
+        }
+        
         for i in sevedRoom{
             if i.x == x && i.y == y{
-                try! realm.write {
-                    realm.delete(i)
+                try! realm?.write {
+                    realm?.delete(i)
                 }
             }
         }
-
+//        print(saveRoom.name)
         
-        saveRoom.id = id
-        saveRoom.name = nameRoom
-        saveRoom.close = close
-        saveRoom.firstVisitingTriger = firstVisitingTriger
         
-        saveRoom.x = x
-        saveRoom.y = y
         
-        saveRoom.up     = Doors["Up"]!.rawValue
-        saveRoom.right  = Doors["Right"]!.rawValue
-        saveRoom.down   = Doors["Down"]!.rawValue
-        saveRoom.left   = Doors["Left"]!.rawValue
-        
-        if enemys.count >= 1{
-            saveRoom.enemy0 = enemys[0].name
-        }
-        if enemys.count >= 2{
-            saveRoom.enemy1 = enemys[1].name
-        }
-        if enemys.count >= 3{
-            saveRoom.enemy2 = enemys[2].name
-        }
-        
-        if inRoom{
-            for i in sevedRoom{
-                if i.name == "0"{
-                    try! realm.write {
-                        realm.delete(i)
-                    }
-                }
-            }
-            let a = SaveRoom()
-            a.name = "0"
-            a.x = x
-            a.y = y
-            try! realm.write {
-                realm.add(a)
-            }
-        }
-        
-        try! realm.write {
-            realm.add(saveRoom)
+        try! realm!.write {
+            realm!.add(saveRoom)
         }
         
     }

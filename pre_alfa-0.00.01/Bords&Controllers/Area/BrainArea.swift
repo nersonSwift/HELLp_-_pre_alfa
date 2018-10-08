962d00acf10ea66fa2fb224b08b9601e2d348a58
nersonSwift <42254802+nersonSwift@users.noreply.github.com>
2018-10-08 20:07:37 +0300

    Revert "Revert "Revert "Revert "Revert "не большие правики"""""
    This reverts commit d01471e8f210071559bb972ff8898725d92ab1cd.

--- pre_alfa-0.00.01/Bords&Controllers/Area/BrainArea.swift
+++ pre_alfa-0.00.01/Bords&Controllers/Area/BrainArea.swift
@@ -79,12 +79,15 @@ class BrainArea {
         thisRoom.saveThisRoom(realm: castPlayer.realm, sevedRoom: castPlayer.savedRooms)
         
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> parent of c3c9056... не большие правики
 =======
 >>>>>>> parent of c3c9056... не большие правики
 =======
         thisRoom.saveThisRoom(realm: castPlayer.realm, sevedRoom: castPlayer.savedRooms)
 >>>>>>> parent of eba7765... 1
+=======
+>>>>>>> parent of c3c9056... не большие правики
     }
     
     private func animStep(dir: Dir, area: Area){
@@ -176,10 +179,13 @@ class BrainArea {
         thisRoom.NoInRoom()
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         thisRoom = castPlayer.map.mapRooms[xy]!
 <<<<<<< HEAD
      //   thisRoom.saveThisRoom(realm: castPlayer.realm, sevedRoom: castPlayer.savedRooms)
 =======
+=======
+>>>>>>> parent of c3c9056... не большие правики
 =======
 >>>>>>> parent of c3c9056... не большие правики
         
@@ -199,11 +205,14 @@ class BrainArea {
         }
         
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> parent of c3c9056... не большие правики
 =======
 >>>>>>> parent of c3c9056... не большие правики
 =======
 >>>>>>> parent of eba7765... 1
+=======
+>>>>>>> parent of c3c9056... не большие правики
         thisRoom.InRoom(castPlayer: castPlayer)
         
         animStep(dir: dir, area: area)
